8c7f3cfe54dbe3b8bea656954aeb4aed02fc0c89
antropovaa <anusy.99@yandex.ru>
2017-09-11 20:53:32 +0300

    two tasks are done

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -62,7 +62,14 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val const: Double = 4.445 / 100
+    val vershoksInSagenes: Int = sagenes * 48
+    val vershoksInArshins: Int = arshins * (48 / 3)
+    val result: Double = vershoksInArshins + vershoksInSagenes + vershoks
+
+    return result * const
+}
 
 /**
  * Тривиальная
