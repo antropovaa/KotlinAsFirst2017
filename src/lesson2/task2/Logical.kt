317609bda73987b6e2dd92a916626d87b1851b66
antropovaa <anusy.99@yandex.ru>
2017-09-18 20:23:08 +0300

    All tasks are done!

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,6 +2,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import java.lang.Math.*
 
 /**
  * Пример
@@ -17,7 +18,14 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val num1 = number % 10000 / 1000
+    val num2 = number % 1000 / 100
+    val num3 = number % 100 / 10
+    val num4 = number % 10
+    return if (num1 + num2 == num3 + num4) true
+    else false
+}
 
 /**
  * Простая
@@ -26,7 +34,10 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return if ((x1 == x2) || (y1 == y2) || (abs(x1 - x2) == abs(y1 - y2))) true
+    else false
+}
 
 /**
  * Средняя
@@ -36,7 +47,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    return if (sqrt(sqr(abs(x1 - x2)) + sqr(abs(y1-y2))) <= (r2-r1)) true
+    else false
+}
 
 /**
  * Средняя
@@ -47,4 +61,10 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    val maxLength = max(max(a,b),c)
+    val minLength = min(min(a,b),c)
+    val midLength = a + b + c - (maxLength + minLength)
+    return if ((minLength <= min(r,s)) && (midLength <= max(r,s))) true
+    else false
+}
